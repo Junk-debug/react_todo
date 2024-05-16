83a6b00ac12c99b8d491edf19cf3ccc714266e3a
macsbook <maks.naumovich@gmail.com>
2024-05-16 15:19:45 +0200

    refactor logic to use editable span again, add some cosmetic features, fix bug with dblclck to edit

--- client/src/components/TODOItem.tsx
+++ client/src/components/TODOItem.tsx
@@ -1,10 +1,9 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { TODOItemType } from "../todo";
 import CloseIcon from "../../public/icons/close.svg?react";
 import EditIcon from "../../public/icons/edit.svg?react";
 import IconButton from "./buttons/IconButton";
 import CheckBox from "./buttons/CheckBox";
-import AdjustableInput from "./AdjustableInput";
 
 interface TODOItemProps {
   item: TODOItemType;
@@ -15,44 +14,69 @@ const TODOItem: React.FC<TODOItemProps> = ({ item, onEditClick }) => {
   const [checked, setChecked] = useState<boolean>(item.checked);
   const [isEditable, setIsEditable] = useState<boolean>(false);
   const [value, setValue] = useState<string>(item.name);
-  const inputRef = useRef<HTMLInputElement>(null);
+  const spanRef = useRef<HTMLSpanElement>(null);
 
-  const coursorToEnd = () => {
-    if (inputRef.current) {
-      inputRef.current.focus();
-      inputRef.current.setSelectionRange(
-        inputRef.current.value.length,
-        inputRef.current.value.length
-      );
+  const focusAndCoursorToEnd = () => {
+    if (spanRef.current) {
+      spanRef.current.focus();
+
+      const content = spanRef.current?.textContent;
+      const range = document.createRange();
+
+      if (content) {
+        range.setStart(spanRef.current.childNodes[0], content.length);
+        range.setEnd(spanRef.current.childNodes[0], content.length);
+      }
+
+      const selection = window.getSelection();
+
+      if (selection) {
+        selection.removeAllRanges();
+        selection.addRange(range);
+      }
     }
   };
 
   const onDoubleClick = () => {
     setIsEditable(true);
-    coursorToEnd();
   };
+
+  useEffect(() => {
+    if (isEditable) {
+      focusAndCoursorToEnd();
+    }
+  }, [isEditable]);
 
   return (
     <>
       <div
         onDoubleClick={onDoubleClick}
-        className="group flex min-h-[40px] h-fit border rounded-md justify-between items-center px-2"
+        className="group flex items-baseline py-[6px] border rounded-md justify-between px-2 select-none"
       >
         <div className="flex items-baseline gap-2">
           <CheckBox
+            className="shrink-0"
             checked={checked}
             onClick={() => setChecked((prev) => !prev)}
           />
-          <AdjustableInput
-            ref={inputRef}
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
+
+          <span
+            ref={spanRef}
+            onChange={() => {
+              if (spanRef.current?.textContent) {
+                setValue(spanRef.current.textContent);
+              }
+            }}
             onBlur={() => setIsEditable(false)}
-            readOnly={!isEditable}
-            className={`${
+            contentEditable={isEditable ? "plaintext-only" : false}
+            suppressContentEditableWarning={true}
+            style={{ wordBreak: "break-word" }}
+            className={`${isEditable ? "text-gray-500" : ""} ${
               checked ? "line-through opacity-50" : ""
-            } focus-visible:outline-none`}
-          />
+            } focus-visible:outline-none shrink`}
+          >
+            {value}
+          </span>
         </div>
         <div className="flex gap-[5px] invisible group-hover:visible">
           <IconButton
